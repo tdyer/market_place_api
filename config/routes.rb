da21554af764a22753a01ef03a39792d465546bd
Tom Dyer <tdyer1@gmail.com>
2015-11-02 10:09:03 -0500

    Adding version to api routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   # API Definition
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/'  do
-    # List our resources here.
-
+    scope module: :v1 do
+      # List our resources here.
+    end
   end
 end

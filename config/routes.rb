f30093ea649aadbff37b9a62887c35a23fbe4bed
Tom Dyer <tdyer1@gmail.com>
2015-11-02 10:57:50 -0500

    Added API Constraint for versioning.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
 
   # API Definition
   namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/'  do
-    scope module: :v1 do
+    scope module: :v1,
+          constaints: ApiConstraints.new(version: 1, default: true) do
       # List our resources here.
     end
   end
